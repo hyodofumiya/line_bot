54c788608df8ad8e131ab1bf81ebe7b16f0fa8de
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-01 01:58:54 +0900

    ユーザー登録フォームの送信段階を見直し

--- app/assets/javascripts/user_new.js
+++ app/assets/javascripts/user_new.js
@@ -1,36 +1,42 @@
 window.addEventListener('turbolinks:load', initializeLiff)
-
+//LIFFを起動----------------------------------------------------------------------------------
 function initializeLiff() {
-  //var userId = data.context.userId
   console.log("test1");
-  myLiffId= "1654154094-L2PYjd9P";
+  MyLiffId= "1654154094-L2PYjd9P";
   liff
     .init({
-      liffId: myLiffId
+      liffId: MyLiffId
     })
     .then(() => {
-      initializeApp();
+      sendMessage();
     })
     .catch((err) => {
       console.log(err.code, err.message);
     });
 }
-
-function initializeApp(){
-  //送信
+//LIFFの機能------------------------------------------------------------------------------------
+function sendMessage(){
+  //メッセージ送信機能
   document.getElementById("sendMessageButton").addEventListener('click', function(){
-    liff.sendMessages([
-    {
-      type:'text',
-      text:'Hello, World!'
+    var familyName=document.getElementById("familyName").value;
+    var firstName=document.getElementById("firstName").value;
+    var employeeNumber=document.getElementById("employeeNumber").value;
+    //htmlでフォームのバリデーションに引っかかったらtrueが入る
+    var checkValid=document.getElementById('signup_form').checkValidity();
+    //バリデーションが問題なければ送信するかどうかの判断をする
+    if (checkValid=="true"){
+      liff.sendMessages([
+      {
+        type:'text',
+        text:familyName
+      }
+      ])
+      .then(() => {
+        console.log('message sent');
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
     }
-    ])
-    .then(() => {
-      console.log('message sent');
-    })
-    .catch((err) => {
-      console.log('error', err);
-    });
   })
 }
-
