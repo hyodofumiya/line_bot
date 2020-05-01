b82febe0051b25fcf9716e11a9e9ab56c738dbaa
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-01 19:17:04 +0900

    jsのメッセージ送信用のオブジェクトの中身を修正

--- app/assets/javascripts/user_new.js
+++ app/assets/javascripts/user_new.js
@@ -1,4 +1,135 @@
 window.addEventListener('turbolinks:load', initializeLiff)
+
+const NewUserSendMessage=
+{
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "direction": "ltr",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "入力内容の確認",
+          "margin": "none",
+          "size": "xl",
+          "align": "center",
+          "weight": "bold",
+          "color": "#767474"
+        },
+        {
+          "type": "separator"
+        }
+      ]
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "flex": 0,
+      "spacing": "none",
+      "margin": "none",
+      "contents": [
+        {
+          "type": "text",
+          "text": "氏名",
+          "size": "lg"
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "text",
+              "text": "姓:　",
+              "flex": 0,
+              "align": "start",
+              "weight": "regular",
+              "color": "#787878"
+            },
+            {
+              "type": "text",
+              "text": "familyName",
+              "align": "start",
+              "weight": "regular",
+              "wrap": true
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "text",
+              "text": "名:　",
+              "flex": 0,
+              "margin": "none",
+              "color": "#787878"
+            },
+            {
+              "type": "text",
+              "text": "firstName",
+              "align": "start",
+              "wrap": true
+            }
+          ]
+        },
+        {
+          "type": "text",
+          "text": "社員番号",
+          "margin": "md",
+          "size": "lg"
+        },
+        {
+          "type": "text",
+          "text": "employeeNumber"
+        },
+        {
+          "type": "text",
+          "text": "上記内容で登録しますか？",
+          "margin": "xxl",
+          "wrap": true
+        }
+      ]
+    },
+    "footer": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "button",
+          "action": {
+            "type": "message",
+            "label": "はい",
+            "text": "はい"
+          },
+          "style": "primary"
+        },
+        {
+          "type": "separator",
+          "margin": "md",
+          "color": "#FFFFFF"
+        },
+        {
+          "type": "button",
+          "action": {
+            "type": "message",
+            "label": "いいえ",
+            "text": "いいえ"
+          },
+          "style": "secondary"
+        }
+      ]
+    }
+  }
+};
+
+
+
+
 //LIFFを起動----------------------------------------------------------------------------------
 function initializeLiff() {
   console.log("test1");
@@ -23,11 +154,13 @@ function sendMessage(){
     employeeNumber=document.getElementById("employeeNumber").value;
     //htmlでフォームのバリデーションに引っかかったらtrueが入る
     var checkValid=document.getElementById('signup_form').checkValidity();
+
     //バリデーションが問題なければ送信するかどうかの判断をする
     if (checkValid==true){
-      liff.sendMessages([
-        set_message(familyName, firstName, employeeNumber)
-      ])
+      var settedObj = set_message(familyName, firstName, employeeNumber);
+      liff.sendMessages(
+        settedObj
+      )
       .then(() => {
         console.log('message sent');
       })
@@ -36,138 +169,16 @@ function sendMessage(){
       });
     }
   })
+
+  function set_message(familyName, firstName, employeeNumber){
+    var obj =NewUserSendMessage;
+    obj.contents.body.contents[1].contents[1].text = familyName;
+    obj.contents.body.contents[2].contents[1].text = firstName;
+    obj.contents.body.contents[4].text = employeeNumber;
+    return obj;
+  }
+
 }
 
-function set_message(familyName, firstName, employeeNumber){
-  var newUserSendMessage=
-  {
-    "type": "flex",
-    "altText": "Flex Message",
-    "contents": {
-      "type": "bubble",
-      "direction": "ltr",
-      "header": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "入力内容の確認",
-            "margin": "none",
-            "size": "xl",
-            "align": "center",
-            "weight": "bold",
-            "color": "#767474"
-          },
-          {
-            "type": "separator"
-          }
-        ]
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "flex": 0,
-        "spacing": "none",
-        "margin": "none",
-        "contents": [
-          {
-            "type": "text",
-            "text": "氏名",
-            "size": "lg"
-          },
-          {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "姓:　",
-                "flex": 0,
-                "align": "start",
-                "weight": "regular",
-                "color": "#787878"
-              },
-              {
-                "type": "text",
-                "text": "familyName",
-                "align": "start",
-                "weight": "regular",
-                "wrap": true
-              }
-            ]
-          },
-          {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "名:　",
-                "flex": 0,
-                "margin": "none",
-                "color": "#787878"
-              },
-              {
-                "type": "text",
-                "text": "firstName",
-                "align": "start",
-                "wrap": true
-              }
-            ]
-          },
-          {
-            "type": "text",
-            "text": "社員番号",
-            "margin": "md",
-            "size": "lg"
-          },
-          {
-            "type": "text",
-            "text": "employeeNumber"
-          },
-          {
-            "type": "text",
-            "text": "上記内容で登録しますか？",
-            "margin": "xxl",
-            "wrap": true
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "message",
-              "label": "はい",
-              "text": "はい"
-            },
-            "style": "primary"
-          },
-          {
-            "type": "separator",
-            "margin": "md",
-            "color": "#FFFFFF"
-          },
-          {
-            "type": "button",
-            "action": {
-              "type": "message",
-              "label": "いいえ",
-              "text": "いいえ"
-            },
-            "style": "secondary"
-          }
-        ]
-      }
-    }
-  };
-  
-  newUserSendMessage.contents.body.contents[1].contents[1].text = familyName;
-  newUserSendMessage.contents.body.contents[2].contents[1].text = firstName;
-  newUserSendMessage.contents.body.contents[4].text = employeeNumber;
-  return newUserSendMessage;
-}
+
+
