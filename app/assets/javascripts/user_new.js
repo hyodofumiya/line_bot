62022d2ff1ae6965101bc76012dda209810e8147
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-01 18:22:09 +0900

    フォーム送信後の表示内容を修正

--- app/assets/javascripts/user_new.js
+++ app/assets/javascripts/user_new.js
@@ -18,18 +18,15 @@ function initializeLiff() {
 function sendMessage(){
   //メッセージ送信機能
   document.getElementById("sendMessageButton").addEventListener('click', function(){
-    var familyName=document.getElementById("familyName").value;
-    var firstName=document.getElementById("firstName").value;
-    var employeeNumber=document.getElementById("employeeNumber").value;
+    familyName=document.getElementById("familyName").value;
+    firstName=document.getElementById("firstName").value;
+    employeeNumber=document.getElementById("employeeNumber").value;
     //htmlでフォームのバリデーションに引っかかったらtrueが入る
     var checkValid=document.getElementById('signup_form').checkValidity();
     //バリデーションが問題なければ送信するかどうかの判断をする
     if (checkValid==true){
       liff.sendMessages([
-      {
-        type:'text',
-        text:"Hello, World!${familyName}"
-      }
+        set_message(familyName, firstName, employeeNumber)
       ])
       .then(() => {
         console.log('message sent');
@@ -39,4 +36,138 @@ function sendMessage(){
       });
     }
   })
+}
+
+function set_message(familyName, firstName, employeeNumber){
+  var newUserSendMessage=
+  {
+    "type": "flex",
+    "altText": "Flex Message",
+    "contents": {
+      "type": "bubble",
+      "direction": "ltr",
+      "header": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "入力内容の確認",
+            "margin": "none",
+            "size": "xl",
+            "align": "center",
+            "weight": "bold",
+            "color": "#767474"
+          },
+          {
+            "type": "separator"
+          }
+        ]
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "flex": 0,
+        "spacing": "none",
+        "margin": "none",
+        "contents": [
+          {
+            "type": "text",
+            "text": "氏名",
+            "size": "lg"
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "姓:　",
+                "flex": 0,
+                "align": "start",
+                "weight": "regular",
+                "color": "#787878"
+              },
+              {
+                "type": "text",
+                "text": "familyName",
+                "align": "start",
+                "weight": "regular",
+                "wrap": true
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "名:　",
+                "flex": 0,
+                "margin": "none",
+                "color": "#787878"
+              },
+              {
+                "type": "text",
+                "text": "firstName",
+                "align": "start",
+                "wrap": true
+              }
+            ]
+          },
+          {
+            "type": "text",
+            "text": "社員番号",
+            "margin": "md",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "employeeNumber"
+          },
+          {
+            "type": "text",
+            "text": "上記内容で登録しますか？",
+            "margin": "xxl",
+            "wrap": true
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "はい",
+              "text": "はい"
+            },
+            "style": "primary"
+          },
+          {
+            "type": "separator",
+            "margin": "md",
+            "color": "#FFFFFF"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "いいえ",
+              "text": "いいえ"
+            },
+            "style": "secondary"
+          }
+        ]
+      }
+    }
+  };
+  
+  newUserSendMessage.contents.body.contents[1].contents[1].text = familyName;
+  newUserSendMessage.contents.body.contents[2].contents[1].text = firstName;
+  newUserSendMessage.contents.body.contents[4].text = employeeNumber;
+  return newUserSendMessage;
 }
