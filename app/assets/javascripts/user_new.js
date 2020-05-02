33dc65d7403f1d527c37c642e51ed867f5c504c6
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-02 19:53:41 +0900

    メッセージを送信すると同じ内容が表示されるようになった

--- app/assets/javascripts/user_new.js
+++ app/assets/javascripts/user_new.js
@@ -1,30 +1,66 @@
 window.addEventListener('turbolinks:load', initializeLiff)
 
-const NewUserSendMessage=
+
+
+
+//LIFFを起動----------------------------------------------------------------------------------
+function initializeLiff() {
+  console.log("test1");
+  MyLiffId= "1654154094-L2PYjd9P";
+  liff
+    .init({
+      liffId: MyLiffId
+    })
+    .then(() => {
+      sendMessage();
+    })
+    .catch((err) => {
+      console.log(err.code, err.message);
+    });
+}
+//LIFFの機能------------------------------------------------------------------------------------
+function sendMessage(){
+  //メッセージ送信機能
+  document.getElementById("sendMessageButton").addEventListener('click', function(){
+    //htmlでフォームのバリデーションに引っかかったらfalse,問題なければtrueが入る
+    var checkValid=document.getElementById('signup_form').checkValidity();
+    //バリデーションが問題なければ送信するかどうかの判断をする
+    if (checkValid==true){
+      var settedObj = set_message();
+      var messageArray = [settedObj];
+      liff.sendMessages(
+        messageArray
+      )
+      .then(() => {
+        console.log('message sent');
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+    }
+  })
+
+  function set_message(){
+    var familyName=document.getElementById("familyName").value;
+    var firstName=document.getElementById("firstName").value;
+    var employeeNumber=document.getElementById("employeeNumber").value;
+    var obj =newUserSendMessage;
+    obj.contents.body.contents[1].contents[1].text = familyName;
+    obj.contents.body.contents[2].contents[1].text = firstName;
+    obj.contents.body.contents[4].text = employeeNumber;
+    return obj;
+  }
+
+}
+
+
+const newUserSendMessage=
 {
   "type": "flex",
   "altText": "Flex Message",
   "contents": {
     "type": "bubble",
     "direction": "ltr",
-    "header": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "text",
-          "text": "入力内容の確認",
-          "margin": "none",
-          "size": "xl",
-          "align": "center",
-          "weight": "bold",
-          "color": "#767474"
-        },
-        {
-          "type": "separator"
-        }
-      ]
-    },
     "body": {
       "type": "box",
       "layout": "vertical",
@@ -86,100 +122,10 @@ const NewUserSendMessage=
         {
           "type": "text",
           "text": "employeeNumber"
-        },
-        {
-          "type": "text",
-          "text": "上記内容で登録しますか？",
-          "margin": "xxl",
-          "wrap": true
-        }
-      ]
-    },
-    "footer": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "button",
-          "action": {
-            "type": "message",
-            "label": "はい",
-            "text": "はい"
-          },
-          "style": "primary"
-        },
-        {
-          "type": "separator",
-          "margin": "md",
-          "color": "#FFFFFF"
-        },
-        {
-          "type": "button",
-          "action": {
-            "type": "message",
-            "label": "いいえ",
-            "text": "いいえ"
-          },
-          "style": "secondary"
         }
       ]
     }
   }
 };
-
-
-
-
-//LIFFを起動----------------------------------------------------------------------------------
-function initializeLiff() {
-  console.log("test1");
-  MyLiffId= "1654154094-L2PYjd9P";
-  liff
-    .init({
-      liffId: MyLiffId
-    })
-    .then(() => {
-      sendMessage();
-    })
-    .catch((err) => {
-      console.log(err.code, err.message);
-    });
-}
-//LIFFの機能------------------------------------------------------------------------------------
-function sendMessage(){
-  //メッセージ送信機能
-  document.getElementById("sendMessageButton").addEventListener('click', function(){
-    familyName=document.getElementById("familyName").value;
-    firstName=document.getElementById("firstName").value;
-    employeeNumber=document.getElementById("employeeNumber").value;
-    //htmlでフォームのバリデーションに引っかかったらtrueが入る
-    var checkValid=document.getElementById('signup_form').checkValidity();
-
-    //バリデーションが問題なければ送信するかどうかの判断をする
-    if (checkValid==true){
-      var settedObj = set_message(familyName, firstName, employeeNumber);
-      var messageArray = [settedObj];
-      liff.sendMessages(
-        messageArray
-      )
-      .then(() => {
-        console.log('message sent');
-      })
-      .catch((err) => {
-        console.log('error', err);
-      });
-    }
-  })
-
-  function set_message(familyName, firstName, employeeNumber){
-    var obj =NewUserSendMessage;
-    obj.contents.body.contents[1].contents[1].text = familyName;
-    obj.contents.body.contents[2].contents[1].text = firstName;
-    obj.contents.body.contents[4].text = employeeNumber;
-    return obj;
-  }
-
-}
-
 
 
