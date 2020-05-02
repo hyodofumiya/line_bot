70ef11c1cf8cefdc76f48ee6c32cc57c3ee12df4
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-02 21:02:34 +0900

    コントローラーとルーティングにuser/user_check_botと追加
    ルーティングとコントローラーにユーザーフォーム送信後の処理を追加

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,16 @@
 class UserController < ApplicationController
+  require 'line/bot'
+  protect_from_forgery :except => [:callback]
+
   def new
   end
 
   def create
+  end
+
+  def user_check_bot
+    binding.pry
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post '/callback' => 'linebot#callback'
   get '/usernew' => 'user#new'
   post '/usercreate' => 'user#create'
+  post '/user/user_check_bot' => 'user#user_check_bot'
 end
