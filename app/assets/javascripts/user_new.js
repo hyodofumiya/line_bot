8f09caf1d7140cb164a6ad4932093c8cc666e5f6
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-02 21:34:50 +0900

    userIdのトークンをフォームに入れて送信することに成功

--- app/assets/javascripts/user_new.js
+++ app/assets/javascripts/user_new.js
@@ -1,8 +1,5 @@
 window.addEventListener('turbolinks:load', initializeLiff)
 
-
-
-
 //LIFFを起動----------------------------------------------------------------------------------
 function initializeLiff() {
   console.log("test1");
@@ -26,106 +23,11 @@ function sendMessage(){
     var checkValid=document.getElementById('signup_form').checkValidity();
     //バリデーションが問題なければ送信するかどうかの判断をする
     if (checkValid==true){
-      var settedObj = set_message();
-      var messageArray = [settedObj];
-      liff.sendMessages(
-        messageArray
-      )
-      .then(() => {
-        console.log('message sent');
-      })
-      .catch((err) => {
-        console.log('error', err);
-      });
+      var userIdToken = liff.getIDToken();
+      document.getElementById("userIdToken").value = userIdToken;
     }
   })
-
-  function set_message(){
-    var familyName=document.getElementById("familyName").value;
-    var firstName=document.getElementById("firstName").value;
-    var employeeNumber=document.getElementById("employeeNumber").value;
-    var obj =newUserSendMessage;
-    obj.contents.body.contents[1].contents[1].text = familyName;
-    obj.contents.body.contents[2].contents[1].text = firstName;
-    obj.contents.body.contents[4].text = employeeNumber;
-    return obj;
-  }
-
 }
 
-
-const newUserSendMessage=
-{
-  "type": "flex",
-  "altText": "Flex Message",
-  "contents": {
-    "type": "bubble",
-    "direction": "ltr",
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "flex": 0,
-      "spacing": "none",
-      "margin": "none",
-      "contents": [
-        {
-          "type": "text",
-          "text": "氏名",
-          "size": "lg"
-        },
-        {
-          "type": "box",
-          "layout": "horizontal",
-          "contents": [
-            {
-              "type": "text",
-              "text": "姓:　",
-              "flex": 0,
-              "align": "start",
-              "weight": "regular",
-              "color": "#787878"
-            },
-            {
-              "type": "text",
-              "text": "familyName",
-              "align": "start",
-              "weight": "regular",
-              "wrap": true
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "horizontal",
-          "contents": [
-            {
-              "type": "text",
-              "text": "名:　",
-              "flex": 0,
-              "margin": "none",
-              "color": "#787878"
-            },
-            {
-              "type": "text",
-              "text": "firstName",
-              "align": "start",
-              "wrap": true
-            }
-          ]
-        },
-        {
-          "type": "text",
-          "text": "社員番号",
-          "margin": "md",
-          "size": "lg"
-        },
-        {
-          "type": "text",
-          "text": "employeeNumber"
-        }
-      ]
-    }
-  }
-};
 
 
