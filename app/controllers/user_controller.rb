4b5f1a5e0bbe3592013487407f9018f1658eeaf6
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-03 01:50:43 +0900

    net/https通信を実行できるようになった

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,8 @@
 class UserController < ApplicationController
   require 'line/bot'
-  protect_from_forgery :except => [:callback]
+  require 'net/https'
+  require 'uri'
+  require 'json'
 
   def new
   end
@@ -9,11 +11,27 @@ class UserController < ApplicationController
   end
 
   def user_check_bot
-    binding.pry
-
+    user_id_token = params[:user_token]
+    get_user_id_from_token(user_id_token)
   end
 
   private
+  #userIDのトークンからuserIDを取得する関数
+  def get_user_id_from_token(user_id_token)
+    uri = URI.parse('https://api.line.me/oauth2/v2.1/verify')
+    http = Net::HTTP.new(uri.host, uri.port)
+
+
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    req = Net::HTTP::Post.new(uri.path)
+
+    req.set_form_data({"id_token":user_id_token, "client_id":"1654154094"})
+    binding.pry
+    res = http.request(req)
+    binding.pry
+  end
 
 end
 
