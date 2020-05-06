c355be519e02b72453e6f91f573e2f84d7f537c7
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-06 22:06:44 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  post '/callback' => 'linebot#callback'
+  post '/callback' => 'linebots#callback'
   get '/usernew' => 'user#new'
   post '/usercreate' => 'user#create'
   post '/user/user_check_bot' => 'user#user_check_bot'
-  get 'richmenu/start_work_menu' => 'richmenu#start_work_menu'
-  get 'test' => 'richmenu#test'
+  get 'richmenu/start_work_menu' => 'richmenus#start_work_menu'
+
+  resources :standby
 end
