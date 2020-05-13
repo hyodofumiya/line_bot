340daa52eed88c42f2a4c99f112a1975ed3a663d
hyodofumiya <f.hyodo.0123@gmail.com>
2020-05-13 22:20:53 +0900

    勤務履歴の一覧表示機能完成（今月と先月分）

--- app/models/time_card.rb
+++ app/models/time_card.rb
@@ -1,8 +1,6 @@
 class TimeCard < ApplicationRecord
   belongs_to :user
 
-
-
   def self.create_new_record_flow(work_time, standby)
     time_card = TimeCard.find_by(user_id: $user.id, date: standby.date)
     #ユーザーのレコードが同日に被っていないか確認する
@@ -39,6 +37,271 @@ class TimeCard < ApplicationRecord
       message = "勤務履歴\n\n日付：#{selected_date.strftime("%Y")}年 #{selected_date.strftime("%m")}月#{selected_date.strftime("%d")}日 \n\n休日"
     end
     return message
+  end
+
+  def self.index_selected_date
+    this_month = Time.now.to_date.all_month
+    last_month = Time.now.last_month.to_date.all_month
+    binding.pry
+    this_month_timecards = TimeCard.where(user_id: $user.id, date: this_month).order(date: :asc)
+    last_month_timecards = TimeCard.where(user_id: $user.id, date: last_month).order(date: :asc)
+    binding.pry
+    this_month_timecards_message = TimeCard.create_timecards_index_message(this_month_timecards, this_month, Time.now.to_date)
+    last_month_timecards_message = TimeCard.create_timecards_index_message(last_month_timecards, last_month, Time.now.last_month.to_date)
+    binding.pry
+    this_and_last_month_timecards_message = TimeCard.compile_this_and_last_month_timecards_message(this_month_timecards_message, last_month_timecards_message)
+    return this_and_last_month_timecards_message
+  end
+
+  private
+
+  def self.create_timecards_index_message(timecards,all_month, year_and_month)
+    #休日も含めた勤怠の記録がまとまった配列を作成
+    message_parts = []
+    for date in all_month do
+      timecard = timecards.find_by(date: date)
+      if timecard.present?
+       message_part = TimeCard.record_into_message(date,timecard)
+      else
+        message_part = TimeCard.not_record_into_message(date)
+      end
+      message_parts << message_part
+    end
+    return self.compile_a_month_timecards_message(message_parts, year_and_month)
+    puts @num
+  end
+
+  def self.record_into_message(date,timecard)
+    message =  {
+      "type": "box",
+      "layout": "horizontal",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "#{date.day}日",
+              "size": "md",
+              "align": "end"
+            }
+          ],
+          "width": "15%"
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "#{timecard.work_time/(60*60)}時間",
+                  "align": "end"
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "#{timecard.work_time%(60*60)/60}分",
+                  "align": "end"
+                }
+              ],
+              "margin": "none",
+              "width": "35%"
+            }
+          ],
+          "width": "40%"
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "#{timecard.break_time/(60*60)}時間",
+                  "align": "end"
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "#{timecard.break_time%(60*60)/60}分",
+                  "align": "end"
+                }
+              ],
+              "width": "35%"
+            }
+          ],
+          "width": "40%"
+        }
+      ]
+    }
+    return message
+  end
+
+  def self.not_record_into_message(date)
+    {
+      "type": "box",
+      "layout": "horizontal",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "#{date.day}日",
+              "size": "md",
+              "align": "end"
+            }
+          ],
+          "width": "15%"
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "休日",
+                  "align": "center"
+                }
+              ]
+            }
+          ],
+          "width": "40%"
+        }
+      ]
+    }
+  end
+
+  def self.compile_a_month_timecards_message(message_parts, year_and_month)
+    compiled_a_month_message = {
+      "type": "bubble",
+      "header": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "#{year_and_month.year}年#{year_and_month.month}月の勤務履歴",
+            "size": "md",
+            "align": "center"
+          }
+        ],
+        "paddingBottom": "0px"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "日付",
+                    "size": "md",
+                    "align": "end",
+                    "decoration": "underline"
+                  }
+                ],
+                "width": "15%"
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": " 作業時間",
+                        "align": "center",
+                        "decoration": "underline"
+                      }
+                    ]
+                  }
+                ],
+                "width": "40%"
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "休憩時間",
+                        "align": "center",
+                        "decoration": "underline"
+                      }
+                    ]
+                  }
+                ],
+                "width": "40%"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": message_parts
+          }
+        ],
+        "margin": "sm"
+      },
+      "styles": {
+        "header": {
+          "separator": true
+        }
+      }
+    }
+  end
+
+  def self.compile_this_and_last_month_timecards_message(this_month_timecards_message, last_month_timecards_message)
+    compiled_this_and_last_month_message = 
+    {
+      "type": "flex",
+      "altText": "Flex Message",
+      "contents": {
+          "type": "carousel",
+          "contents": [
+            this_month_timecards_message,
+            last_month_timecards_message
+          ]
+        }
+    }
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,31 @@
-richmenus = [
-  [1,"出勤用", "richmenu-4357e5ee1155e1a187c752f69b27ecb1", "状態：退勤中"],
-  [2,"休憩開始", "richmenu-c483410ed627718cbda57d6ce91ac7f1", "状態:勤務中"],
-  [3,"休憩終了", "richmenu-83275022a313bfdfaa7fb7ed1d2d834f", "状態：休憩中"],
-  [4,"その他", "richmenu-07792a76bd76607a22553bbf2e052b30", "状態：その他"]
-]
 
-richmenus.each do |richmenu|
-  req = Richmenu.new(id: richmenu[0], name: richmenu[1], richmenu_id: richmenu[2], explanation: richmenu[3], created_at: Time.now, updated_at: Time.now)
-  res = req.save
-  p req
-  p res
+def create_richmenu_data
+    richmenus = [
+    [1,"出勤用", "richmenu-4357e5ee1155e1a187c752f69b27ecb1", "状態：退勤中"],
+    [2,"休憩開始", "richmenu-c483410ed627718cbda57d6ce91ac7f1", "状態:勤務中"],
+    [3,"休憩終了", "richmenu-83275022a313bfdfaa7fb7ed1d2d834f", "状態：休憩中"],
+    [4,"その他", "richmenu-07792a76bd76607a22553bbf2e052b30", "状態：その他"]
+  ]
+
+  richmenus.each do |richmenu|
+    req = Richmenu.new(id: richmenu[0], name: richmenu[1], richmenu_id: richmenu[2], explanation: richmenu[3], created_at: Time.now, updated_at: Time.now)
+    res = req.save
+    p req
+    p res
+  end
 end
+
+def create_dammy_data
+  for num in 2..18 do
+    date = "2020/04/#{num}"
+    date = date.to_date
+    start_time = Time.new(date.year, date.month, date.day, 2, 1, 0)
+    finish_time = Time.new(date.year, date.month, date.day, num+1, 2+num, 0)
+    break_time = 1800
+    work_time = finish_time - start_time - break_time
+    new = TimeCard.new(user_id: 1, date: date, work_time: work_time, start_time: start_time, finish_time: finish_time, break_time: break_time)
+    new.save
+  end
+end
+
+create_dammy_data
