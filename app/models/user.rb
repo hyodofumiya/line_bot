ca47210beba86b23041bb45ff7a8672cefa0ef79
hyodofumiya <f.hyodo.0123@gmail.com>
2020-06-06 16:55:52 +0900

    userモデルにgroupとuser_groupモデルとのリレーションを追加
    userモデルにgroupモデルとその中間テーブルのuser_groupモデルとの中間テーブルを作成した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_one :standby
   has_many :time_cards
+  has_many :user_groups
+  has_many :groups, thought: :user_groups
 
   validates :employee_number, uniqueness: true
   validates :line_id, uniqueness: true
