0385319ffcc274e67f7c10bb97fb500bd52a263e
hyodofumiya <f.hyodo.0123@gmail.com>
2020-07-23 16:08:37 +0900

    余分なレコードを作成しないようにした

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,25 +1,23 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def line; ; end
+  def line
+    basic_action
+  end
 
   private
 
   def basic_action
-    binding.pry
     @omniauth = request.env['omniauth.auth']
     if @omniauth.present?
-      @profile = SocialProfile.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
-      if @profile
-        @profile.set_values(@omniauth)
-        sign_in(:user, @profile.user)
+      user = User.find_by(line_id: @omniauth['uid'])
+      if user
+        bypass_sign_in(user)
+        binding.pry
       else
-        @profile = SocialProfile.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
-        email = @omniauth['info']['email'] ? @omniauth['info']['email'] : Faker::Internet.email
-        @profile.user = current_user || User.create!(email: email, name: @omniauth['info']['name'], password: Devise.friendly_token[0, 20])
-        @profile.set_values(@omniauth)
-        sign_in(:user, @profile.user)
-        redirect_to edit_user_path(@profile.user.id) and return
+        redirect_to new_user_session_path
       end
+    else
+      redirect_to new_user_session_path
     end
-    redirect_to root_path
+    binding.pry
   end
 end
