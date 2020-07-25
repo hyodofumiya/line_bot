50890cb38fa3d32a76d169bde56fc064e7ff4f4f
hyodofumiya <f.hyodo.0123@gmail.com>
2020-07-25 16:23:13 +0900

    socialPlofileの記述残りを削除した

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,11 @@ class User < ApplicationRecord
   has_many :time_cards
   has_many :user_groups
   has_many :groups, through: :user_groups
-  has_many :social_profiles, dependent: :destroy
 
   validates :first_name, presence: true
   validates :family_name, presence: true
   validates :employee_number, uniqueness: true, presence: true, numericality: {greater_than_or_equal_to: 10000, less_than_or_equal_to: 99999}
   validates :line_id, uniqueness: true, presence: true
 
-  def social_profile(provider)
-    social_profiles.select{ |sp| sp.provider == provider.to_s }.first
-  end
+
 end
