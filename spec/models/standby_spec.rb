a6e09f3294eecc76fee0f61873d96ba6df3d06d7
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-06 21:06:33 +0900

    rspecを導入した

---
+++ spec/models/standby_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Standby, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
--- test/models/standby_test.rb
+++ test/models/standby_test.rb
@@ -4,4 +4,7 @@ class StandbyTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  #テスト一覧
+  #
 end
