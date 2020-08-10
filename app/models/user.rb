2d7667173c716554da91e4ef8e6ba3fcb4503fa6
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-10 11:05:08 +0900

    userモデルのバリデーションを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ApplicationRecord
   has_many :groups, through: :user_groups
 
   validates :employee_number, presence: true
-  validates :employee_number, uniqueness: true, numericality: {only_integer: true}, length: {is: 5, message: "5ケタの半角数字で入力してください"}, if: Proc.new { |resource| resource.employee_number.present?}
+  validates :employee_number, uniqueness: true, numericality: {only_integer: true}, length: {is: 5, message: "は5ケタの半角数字で入力してください"}, if: Proc.new { |resource| resource.employee_number.present?}
   validates :family_name, presence: true
-  validates :family_name, format: {with: /\A([ァ-ン]|ー)+\z/, message: "全角カタカナのみ使用できます"}, length: { in: 1..15, too_long: "最大%{count}文字です", too_short: "を入力してください" }, if: Proc.new{ |resource| resource.family_name.present?}
+  validates :family_name, format: {with: /\A([ァ-ン]|ー)+\z/, message: "は全角カタカナのみ使用できます"}, length: { in: 1..15, too_long: "は最大%{count}文字です", too_short: "を入力してください" }, if: Proc.new{ |resource| resource.family_name.present?}
   validates :first_name, presence: true
-  validates :first_name, format: {with: /\A([ァ-ン]|ー)+\z/, message: "全角カタカナのみ使用できます"}, length: { in: 1..15, too_long: "最大%{count}文字です", too_short: "を入力してください" }, if: Proc.new{ |resource| resource.first_name.present?}
+  validates :first_name, format: {with: /\A([ァ-ン]|ー)+\z/, message: "は全角カタカナのみ使用できます"}, length: { in: 1..15, too_long: "は最大%{count}文字です", too_short: "を入力してください" }, if: Proc.new{ |resource| resource.first_name.present?}
   validates :line_id, uniqueness: true, presence: true
   validates :email, allow_blank: true, uniqueness: true, format: {with: /\A\S+@\S+\.\S+\z/, message: "フォーマットが間違っています"}
 end
