c4abda328d0fc82bdff858c5127d9eb6c52b6fb0
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-25 11:56:16 +0900

    userのdashboardに不必要な表示を削除した

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -33,11 +33,9 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  id
   employee_number
   family_name
   first_name
-  line_id
   admin_user
   standby
   ].freeze
