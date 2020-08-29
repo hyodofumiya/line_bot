897e3dcfaace36189dc6c6a5d42f4087c3b87b00
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-29 14:54:34 +0900

    UserモデルにvartualAttrを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :user_groups
   has_many :groups, through: :user_groups
 
+  attr_accessor :on_work
+
   validates :employee_number, presence: true
   validates :employee_number, uniqueness: true, format: {with: /\A([1-9]\d{4})+\z/, message: "は5桁の半角数字で入力してください"}, if: Proc.new{|resource| resource.employee_number.present?}
   validates :family_name, presence: true
@@ -18,5 +20,10 @@ class User < ApplicationRecord
   validates :first_name, format: {with: /\A([ァ-ン]|ー)+\z/, message: "は全角カタカナのみ使用できます"}, length: { in: 1..15, too_long: "は最大%{count}文字です", too_short: "を入力してください" }, if: Proc.new{ |resource| resource.first_name.present?}
   validates :line_id, uniqueness: true, presence: true
   validates :email, uniqueness: true, format: {with: /\A\S+@\S+\.\S+\z/, message: "フォーマットが間違っています"}, if: Proc.new{ |resource| resource.email.present? }
+
+
+  def on_work
+    self.standby.present?
+  end
 
 end
