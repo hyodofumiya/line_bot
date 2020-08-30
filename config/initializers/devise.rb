538e8097722bfc7aa8619806420dffd075b88a56
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-30 11:10:06 +0900

    デプロイエラーに対処

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,11 +260,12 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :line, Rails.application.credentials.line[:key], Rails.application.credentials.line[:secret]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
-  #
+  
   # config.warden do |manager|
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
@@ -297,6 +298,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :line, Rails.application.credentials.line[:key], Rails.application.credentials.line[:secret]
   
 end
