da6a7a4ffbff036600ceb419947a6e6f5c2da0d4
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-31 16:07:20 +0900

    applicationコントローラーのリファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@ class ApplicationController < ActionController::Base
   require 'json'
   protect_from_forgery :except => [:callback]
 
+  #ログイン後の画面を設定
   def after_sign_in_path_for(resource)
     admin_time_cards_path
   end
 
+  #ユーザーの権限に応じてログアウト後のページのパスを返すメソッド
   def after_sign_out_path_for(resource, admin)
     if admin
       new_user_session_path 
