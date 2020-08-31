fd77419544c15f82869442ec61973bd9d691d3ad
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-31 14:27:32 +0900

    admin/time_cardコントローラーのリファクタリング

--- app/controllers/admin/time_cards_controller.rb
+++ app/controllers/admin/time_cards_controller.rb
@@ -1,12 +1,5 @@
 module Admin
   class TimeCardsController < Admin::ApplicationController
-    # Overwrite any of the RESTful controller actions to implement custom behavior
-    # For example, you may want to send an email after a foo is updated.
-    #
-    # def update
-    #   super
-    #   send_foo_updated_email(requested_resource)
-    # end
     def new
       resource = new_resource
       authorize_resource(resource)
@@ -35,103 +28,45 @@ module Admin
       requested_resource[:break_time] /= 60 if requested_resource[:break_time].present?
       requested_resource[:start_time] = requested_resource[:start_time].strftime("%H:%M") if requested_resource[:start_time].present?
       requested_resource[:finish_time] = requested_resource[:finish_time].strftime("%H:%M") if requested_resource[:finish_time].present?
-      render locals: {
-        page: Administrate::Page::Form.new(dashboard, requested_resource),
-      }
+      render locals: {page: Administrate::Page::Form.new(dashboard, requested_resource)}
     end
 
     def update
       if requested_resource.update(resource_params)
         requested_resource[:break_time] /= 60 if requested_resource[:break_time].present?
-        redirect_to(
-          [namespace, requested_resource],
-          notice: translate_with_resource("update.success"),
-        )
         send_line(User.find(params[:time_card][:user_id]).line_id, "#{params[:line_messsage]}", "#{params[:time_card][:date]}の出勤簿にアクションがありました")
+        redirect_to([namespace, requested_resource], notice: translate_with_resource("update.success"))
       else
         requested_resource[:break_time] /= 60 if requested_resource[:break_time].present?
-        render :edit, locals: {
-          page: Administrate::Page::Form.new(dashboard, requested_resource),
-        }
+        render :edit, locals: {page: Administrate::Page::Form.new(dashboard, requested_resource)}
       end
     end
 
     private
-
+    #ストロングパラメータ
     def resource_params
-      fixed_start_time = "#{params[:time_card][:date]} #{params[:time_card][:start_time]}"
-      fixed_finish_time = "#{params[:time_card][:date]} #{params[:time_card][:finish_time]}"
-      fixed_break_time = params[:time_card][:break_time].present? ? params[:time_card][:break_time].to_i*60 : 0 
-      work_time = fixed_start_time.present? && fixed_finish_time.present? ? ((fixed_finish_time.to_time - fixed_start_time.to_time) - fixed_break_time).to_i : 0
+      fix_resource_params
       params.require(resource_class.model_name.param_key).
         permit(dashboard.permitted_attributes).merge!({"start_time"=>fixed_start_time, "finish_time"=>fixed_finish_time, "work_time"=> "#{work_time}", "break_time"=> fixed_break_time})
     end
 
-    def scoped_resource
-      if @admin
-        resource_class
-      else
-        super.where(user_id: current_user.id)
-      end
+    #DBの型に対応する型にresource情報を整形するメソッド。
+    def fix_resource_params
+      @fixed_start_time = "#{params[:time_card][:date]} #{params[:time_card][:start_time]}"
+      @fixed_finish_time = "#{params[:time_card][:date]} #{params[:time_card][:finish_time]}"
+      @fixed_break_time = params[:time_card][:break_time].present? ? params[:time_card][:break_time].to_i*60 : 0 
+      @work_time = fixed_start_time.present? && fixed_finish_time.present? ? ((fixed_finish_time.to_time - fixed_start_time.to_time) - fixed_break_time).to_i : 0
     end
 
-    #lineにプッシュメッセージを送信するメソッド
-    #第一引数 送信先のline_id
-    #第二引数 送信するメッセージ
-    #第三引数 タイトルに表示するメッセージ　例：出勤簿にアクションが付きました
-    def send_line(user_line_id, return_message, title_message)
-      line_send = params[:line_send]
-      if line_send == "true"
-        client.push_message(user_line_id, change_timecard_message(return_message, title_message))
-      end
-    end
-
-    def change_timecard_message(return_message, title_message)
-      {
-        "type": "flex",
-        "altText": "管理者がアクションしました",
-        "contents":
-        {
-          "type": "bubble",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": title_message,
-                "color": "#C1D21F",
-                "wrap": true,
-              },
-              {
-                "type": "text",
-                "text": return_message,
-                "margin": "lg",
-                "wrap": true,
-              }
-            ]
-          }
-        }
-      }
-    end
-
-
-    def client
-      client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = "d8b577ffcb6bb3447f437c2a6285b27f" #ENV["LINE_CHANNEL_SECRET"]
-        config.channel_token = "S5fTELJVb90Nr4PW9YQcQettd2e7ox4eVHOKpdNXqOs8akh5BVjVLLzfr4EPFVaQsxNqXNZFEhu22kk9/nTI7PrttXwfaQ0PdiXY15W8mJMgjxLBuMAE8fGgu32MdhFjH2jBhad/Ro7T4Y7e5Yx31AdB04t89/1O/w1cDnyilFU="#ENV["LINE_CHANNEL_TOKEN"]
-      }
-    end
-
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
-
+    #勤怠履歴のindexページのレコードの並び順を反映させるカラムを定義
     def default_sorting_attribute
       :date
     end
     
+    #勤怠履歴のindexページのレコードの並び順を定義
     def default_sorting_direction
       :desc
     end
+
   end
 end
