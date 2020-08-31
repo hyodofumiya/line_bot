5c3da11d58ad1cfe29ddf152da3799c257981e39
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-31 16:15:23 +0900

    omniauth_callbackコントローラーのリファクタリング

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,25 +1,25 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
+  #lineログイン時にlineからのコールバックを受け取るメソッド
   def line
     basic_action
   end
 
   private
 
+  #LINEログイン時のリダイレクト先を定義
   def basic_action
     @omniauth = request.env['omniauth.auth']
     if @omniauth.present?
       user = User.find_by(line_id: @omniauth['uid'])
       if user
-        bypass_sign_in(user)
+        bypass_sign_in(user) #ここでdeviseにログイン処理を強制している
         redirect_to admin_time_cards_path
       else
-        @errors = ["社員情報が見つかりません。", "先に社員登録をしてください。"]
-        redirect_to user_session_login_path
+        redirect_to user_session_login_path, alert: "社員情報が見つかりません。"
       end
     else
-      @errors = ["lineログインできませんでした。"]
-      render user_session_login_path
+      redirect_to user_session_login_path, alert: "lineログインできませんでした。"
     end
     
   end
