569ba14753ff046730a277f1f1ca052568ef5680
hyodofumiya <f.hyodo.0123@gmail.com>
2020-08-31 15:38:38 +0900

    admin/standbyコントローラーのリファクタリング

--- app/controllers/admin/standbies_controller.rb
+++ app/controllers/admin/standbies_controller.rb
@@ -4,64 +4,57 @@ module Admin
     def create
       resource = resource_class.new(resource_params)
       if resource.save
-        redirect_to(
-          [namespace, resource],
-          notice: translate_with_resource("create.success"),
-        )
         send_line(User.find(params[:standby][:user_id]).line_id, "#{params[:line_message]}", "出勤情報にアクションがありました。")
+        redirect_to([namespace, resource], notice: translate_with_resource("create.success"))
       else
         resource[:break_sum] /= 60 if resource[:break_sum].present?
-        render action: :new, locals: {
-          page: Administrate::Page::Form.new(dashboard, resource),
-        }
+        binding.pry
+        render action: :new, locals: {page: Administrate::Page::Form.new(dashboard, resource)}
       end
     end
 
     def edit
-      requested_resource[:break_sum] /= 60 if requested_resource[:break_sum].present?
-      render locals: {
-        page: Administrate::Page::Form.new(dashboard, requested_resource),
-      }
+      convert_break_sum_of_reqested_resource_to_min
+      render locals: {page: Administrate::Page::Form.new(dashboard, requested_resource)}
     end
 
     def update
       if requested_resource.update(resource_params)
-        requested_resource[:break_sum] /= 60 if requested_resource[:break_sum].present?
-        redirect_to(
-          [namespace, requested_resource],
-          notice: translate_with_resource("update.success")
-        )
+        convert_break_sum_of_reqested_resource_to_min
+        redirect_to([namespace, requested_resource], notice: translate_with_resource("update.success"))
         send_line(User.find(params[:standby][:user_id]).line_id, "#{params[:line_messsage]}", "勤務状況にアクションがありました")
       else
-        requested_resource[:break_sum] /= 60 if requested_resource[:break_sum].present?
-        render :edit, locals: {
-          page: Administrate::Page::Form.new(dashboard, requested_resource),
-        }
+        convert_break_sum_of_reqested_resource_to_min
+        render :edit, locals: {page: Administrate::Page::Form.new(dashboard, requested_resource)}
       end
     end
 
     private
 
+    #ストロングパラメータ
     def resource_params
-      break_sum = params[:standby][:break_sum].present? ? params[:standby][:break_sum].to_i*60 : 0
-      params.require(resource_class.model_name.param_key).permit(dashboard.permitted_attributes).merge!({"break_sum"=>break_sum})
+      convert_break_sum_to_sec
+      params.require(resource_class.model_name.param_key).permit(dashboard.permitted_attributes).merge!({"break_sum"=>@break_sum})
     end
 
-    def scoped_resource
-      if @admin
-        resource_class
-      else
-        super.where(user_id: current_user.id)
-      end
+    #break_sumの単位をminからsecに変換し、＠break_sumとして定義
+    def convert_break_sum_to_sec
+      @break_sum = params[:standby][:break_sum].present? ? params[:standby][:break_sum].to_i*60 : 0
     end
 
+    #indexページの並び順を決めるカラムを定義
     def default_sorting_attribute
       :start
     end
     
+    #indexページの並び順を定義
     def default_sorting_direction
       :desc
     end
 
+    #requested_resourceのbreak_sumをsecからminに変換する
+    def convert_break_sum_of_reqested_resource_to_min
+      requested_resource[:break_sum] /= 60 if requested_resource[:break_sum].present?
+    end
   end
 end
