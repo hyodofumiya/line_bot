c77bd5d8563a992cbadd9e2bfde1b868dd199e57
hyodofumiya <f.hyodo.0123@gmail.com>
2020-09-04 05:51:21 +0900

    モデルのlineのclientをApplication_recordにまとめた

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  #lineAPIを使用するためのclientを定義
+  def self.client
+    client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = "d8b577ffcb6bb3447f437c2a6285b27f" #ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = "S5fTELJVb90Nr4PW9YQcQettd2e7ox4eVHOKpdNXqOs8akh5BVjVLLzfr4EPFVaQsxNqXNZFEhu22kk9/nTI7PrttXwfaQ0PdiXY15W8mJMgjxLBuMAE8fGgu32MdhFjH2jBhad/Ro7T4Y7e5Yx31AdB04t89/1O/w1cDnyilFU="#ENV["LINE_CHANNEL_TOKEN"]
+    } 
+  end
+
 end
