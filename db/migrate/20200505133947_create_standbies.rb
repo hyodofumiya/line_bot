bb6e318150697ed100a3f7a599c1412a69833c7f
hyodofumiya <f.hyodo.0123@gmail.com>
2020-09-04 18:08:07 +0900

    Standbyのマイグレーションファイルを修正

--- db/migrate/20200505133947_create_standbies.rb
+++ db/migrate/20200505133947_create_standbies.rb
@@ -5,7 +5,7 @@ class CreateStandbies < ActiveRecord::Migration[5.2]
       t.date :date,  null:false
       t.timestamp :start, null:false
       t.timestamp :break_start
-      t.integer :break_sum  #休憩時間の合計をsecondで保存
+      t.integer :break_sum, null:false, default: 0 #休憩時間の合計をsecondで保存
       t.timestamp
     end
   end
