f95581cb90882b4d9afbaff98c1b1b2baecde2b3
hyodofumiya <f.hyodo.0123@gmail.com>
2020-09-05 07:41:54 +0900

    groupモデルのリファクタリング

--- app/models/group.rb
+++ app/models/group.rb
@@ -17,21 +17,21 @@ class Group < ApplicationRecord
     end
     return response
   end
-end
 
-#グループの概要を取得してグルーム名を返すメソッド
-def get_group_name(groupId)
-  uri = URI.parse("https://api.line.me/v2/bot/group/#{groupId}/summary")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = uri.scheme
+  #グループの概要を取得してグルーム名を返すメソッド
+  def get_group_name(groupId)
+    uri = URI.parse("https://api.line.me/v2/bot/group/#{groupId}/summary")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme
 
-  headers = 'Authorization: Bearer{channel access token}' #channel access tokenの部分が認証済みアカウントになると取得できる
-  req = Net::HTTP::GET.new(uri.path)
-  req.initialize_http_header(headers)
+    headers = 'Authorization: Bearer{channel access token}' #channel access tokenの部分が認証済みアカウントになると取得できる
+    req = Net::HTTP::GET.new(uri.path)
+    req.initialize_http_header(headers)
 
-  res = http.request(req)
-  
-  result = ActiveSupport::JSON.decode(res.body)
-  group_name = result["groupName"]
-  return group_name
+    res = http.request(req)
+    
+    result = ActiveSupport::JSON.decode(res.body)
+    group_name = result["groupName"]
+    return group_name
+  end
 end
